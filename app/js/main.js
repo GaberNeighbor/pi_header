3ac7d4999f0c8c41c874c6d14499c7f1f8044125
GaberNeighbor <gabrielalvmil@gmail.com>
2017-02-07 11:23:42 -0500

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -9,17 +9,33 @@ $(document).ready(function(){
 		});
     
         $('#site-cache').click(function(e){
-          $('body').removeClass('with--sidebar');
-          $('#nav-icon1').removeClass('open');
+            $('body').removeClass('with--sidebar');
+            $('#nav-icon1').removeClass('open');
         });
 
-        $('.search__icon').click(function(e){
-            $(this).parent().toggleClass('search--open');
-            $('.search--open > .search__bar > input').focus();
+        $('.search__button').click(function(e){
+            search();
         });
+
+        $(".search__input").keypress(function(e) {
+            if(e.which == 13) {
+                search(); 
+            }
+        });
+
 	})(jQuery);
 
 });
+
+function search() {
+    var input = $('.search__input').val();
+    var win = window.open('https://predictiveindex.force.com/knowledgebase/s/search/All/Home/' + input, '_blank');
+    if (win) {
+        win.focus();
+    } else {
+        alert('Please allow popups for this website');
+    }
+}
 
 function init() {
     window.addEventListener('scroll', function(e){
